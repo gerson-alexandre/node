f9e3756579a7e02776d98e65d56a7ebdb8b19912
Gerson <gerson.x.alexandre@gmail.com>
2020-04-07 21:53:21 +0200

    saving all files

---
+++ routes/view.js
@@ -0,0 +1,19 @@
+var express = require('expresss');
+var router = espress.Router();
+var  pokemon = require('../db.jsoo');
+var request = require('request');
+
+router.get('/:pokeId', function(req, res, next){
+
+    request({
+        url:"http://localhost:3000", req:params.pokeId,
+        method:"GET",
+    }, function(error, response,body){
+        console.log(JSON.parse(body));
+        //send a response
+        res.render('view', {poke: JSON.parse(body)});
+
+    })
+})
+
+module.exports = router;
